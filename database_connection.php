3ea176841fcc57ed458db1010a99945d0ffcd08b
NiiAku31 <77838552+NiiAku31@users.noreply.github.com>
2021-12-11 00:07:26 +0000

    Update database_connection.php

--- database_connection.php
+++ database_connection.php
@@ -1,6 +1,6 @@
 <?php
 
-	$conn = mysqli_connect('localhost','root','Niikwakuaku@31','StormArt');
+	$conn = mysqli_connect('localhost','root','Michaelofori@1234','StormArt');
 
 	if ($conn == false)
 	{
